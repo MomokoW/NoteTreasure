6d3f7fb3684f8461a40a8b78425f7f1afa3ed07e
Momoko_W <1165874815@qq.com>
2019-03-26 14:38:29 +0800

    本次上传修改了项目的基本文件结构，Memo为新建便笺页面，DoList为新建待办事项页面，Diary为新建心情日记页面。同时实现了界面之间的跳转，使用显示意图进行跳转。

--- app/src/main/java/com/example/notes/notestreasure/MainActivity.java
+++ app/src/main/java/com/example/notes/notestreasure/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.notes.notestreasure;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //获取兼容低版本的ActionBar
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("记事宝");
         toolbar.setSubtitle("———记录美好生活");
         setSupportActionBar(toolbar);
+
+    }
+
+    //实现添加便笺按钮跳转事件，使用显示意图进行跳转
+    public void addMemo(View v) {
+        //参数1上下文，参数2跳转Activity的字节码
+        Intent intent = new Intent(getApplicationContext(),MemoActivity.class);
+        startActivity(intent);
+    }
+
+    //实现添加心情日记按钮跳转事件
+    public void addDiary(View v){
+        //参数1上下文，参数2跳转Activity的字节码
+        Intent intent = new Intent(getApplicationContext(),DiaryActivity.class);
+        startActivity(intent);
+    }
+
+    //实现添加待办事项按钮跳转事件
+    public void addDoList(View v){
+        //参数1上下文，参数2跳转Activity的字节码
+        Intent intent = new Intent(getApplicationContext(),DoListActivity.class);
+        startActivity(intent);
     }
 }
